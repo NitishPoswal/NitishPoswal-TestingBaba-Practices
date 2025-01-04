7b6a3a28dbb318ef0ce3137c659518428be9774a
Nitish Kumar <nitishposwal2004@gmail.com>
2025-01-04 17:50:39 +0530

    my final commit

--- src/main/java/testingbBaba_Wadgets_Pages/Slider_Pages.java
+++ src/main/java/testingbBaba_Wadgets_Pages/Slider_Pages.java
@@ -1,7 +1,13 @@
 package testingbBaba_Wadgets_Pages;
 
+import org.openqa.selenium.support.PageFactory;
+
 import baselibrary.BaseLibrary;
 
-public class Slider_Pages extends BaseLibrary {
-
+public class Slider_Pages extends BaseLibrary 
+{
+	public Slider_Pages()
+	{
+		PageFactory.initElements(driver, this);
+	}
 }
