640c0080856efa04ce7168100851fdd1ba577431
Nitish Kumar <nitishposwal2004@gmail.com>
2025-01-08 13:17:55 +0530

    my second commit

--- src/main/java/testingbBaba_Wadgets_Pages/Slider_Pages.java
+++ src/main/java/testingbBaba_Wadgets_Pages/Slider_Pages.java
@@ -1,5 +1,7 @@
 package testingbBaba_Wadgets_Pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import baselibrary.BaseLibrary;
@@ -11,6 +13,27 @@ public class Slider_Pages extends BaseLibrary
 		PageFactory.initElements(driver, this);
 	}
 	
+	 @FindBy(xpath="//*[text()='×']")
+		private WebElement close ;
+		
+     @FindBy(xpath="//*[text()='Practice']")
+		private WebElement prectices ;
+		
+	 @FindBy(xpath="//button[@data-target='#widget']")
+		private WebElement widgets;
+		
+	 @FindBy(xpath = "")
+	 private WebElement slider;
+		
+		
+       public void closetowidgets()
+		{
+			close.click();
+			prectices.click();
+			widgets.click();
+			slider.click();
+		}
+		
 	public void clickevent()
 	{
 		
